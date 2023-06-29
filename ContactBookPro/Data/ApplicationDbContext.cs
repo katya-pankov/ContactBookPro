044386a44298afa7ea7ec072acdfd2042d2beadd
katya-pankov <e.pankov@students.clark.edu>
2023-06-28 18:47:53 -0700

    Add project files.

---
+++ ContactBookPro/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ContactBookPro.Models;
+
+namespace ContactBookPro.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //To generate contacts and categories for our migration 
+        public virtual DbSet<Contact> Contacts { get; set; } = default!;
+        public virtual DbSet<Category> Categories { get; set; } = default!; 
+
+
+    }
+}
---
+++ ContactBookPro/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace ContactBookPro.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? AppUserId { get; set; }
+        [Required]
+        [Display(Name="Category Name")]
+        public string? Name { get; set; }
+
+        //Virtuals (we need a FK for AppUser). 
+        public virtual AppUser? AppUser { get; set; }
+
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+    }
+}
